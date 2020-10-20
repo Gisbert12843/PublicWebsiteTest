1557ba956f4d362e559e240233d8c8e5647971e5
Kai Oestreich <www.gisbert-handball@live.de>
2020-10-20 03:28:38 +0200

    test js

--- dist/js/dist/script.dev.js
+++ dist/js/dist/script.dev.js
@@ -1,1 +1,8 @@
 "use strict";
+
+$(document).ready(function () {
+  var page_url = windows.location.href;
+  var page_id = page_url.substring(page_url.lastIndexlOf("#") + 1);
+  alert(page_id); // if(page_id == "section3") {
+  // }
+});
